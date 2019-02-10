82c5fbc0bb32c1fca9c59ef2c9ed5a0b1d253f02
Krom Stern <kromster80@gmail.com>
2019-02-10 12:39:01 +0300

    Hiding extra things from interface

--- GameThread.pas
+++ GameThread.pas
@@ -25,6 +25,9 @@ type
   // The main thread of application (= KP, it contain access to DLL and also Hands and it react to the basic events)
   TGameThread = class(TThread)
   private
+    fTick: Cardinal;
+    fMaxTick: Cardinal;
+
     fExtAI: TExtAIMain; // ExtAI DLL entry point
     fHands: TList; // ExtAI hand entry point
     fSimState: TSimulationState;
@@ -32,13 +35,13 @@ type
     fOnLog: TLogEvent;
     procedure Log(aLog: wStr);
   public
-    Tick: ui32;
-    MaxTick: Cardinal;
-    property SimulationState: TSimulationState read fSimState;
     constructor Create(aInitLog: TLogEvent; aUpdateSimStatus: TUpdateSimStatus); reintroduce;
     destructor Destroy(); override;
 
     function GetDLLs(aPaths: wStrArr): TListDLL;
+    property SimulationState: TSimulationState read fSimState;
+    property Tick: Cardinal read fTick;
+    property MaxTick: Cardinal read fMaxTick;
 
     procedure InitSimulation(aMultithread: Boolean; aExtAIs: wStrArr; aLogProgress: TLogProgressEvent);
     procedure StartSimulation(aTicks: Cardinal);
@@ -57,8 +60,9 @@ begin
   inherited Create(True);
   FreeOnTerminate := False;
   Priority := tpHigher;
-  Tick := 0;
-  MaxTick := 0;
+
+  fTick := 0;
+  fMaxTick := 0;
   fSimState := ssCreated;
   fOnLog := aInitLog;
   fUpdateSimStatus := aUpdateSimStatus;
@@ -98,7 +102,7 @@ var
 begin
   Log('TMainThread-StartSimulation');
 
-  MaxTick := aTicks;
+  fMaxTick := aTicks;
 
   for L := 0 to fHands.Count-1 do
     if (fHands[L] <> nil) then
@@ -116,14 +120,14 @@ var
 begin
   Log('TMainThread-Execute: Start');
   fSimState := ssInProgress;
-  Tick := 0;
+  fTick := 0;
   gMainData.Tick := Tick;
   SetLength(gMainData.Map,MAP_LENGTH);
-  while (fSimState <> ssTerminated) AND (Tick < MaxTick) do
+  while (fSimState <> ssTerminated) AND (Tick < fMaxTick) do
   begin
     if (fSimState = ssInProgress) then
     begin
-      Inc(Tick);
+      Inc(fTick);
       gMainData.Tick := Tick;
       fUpdateSimStatus(); // Log status
       // Update map
@@ -144,8 +148,9 @@ begin
     else
       Sleep(SLEEP_BEFORE_RUN);
   end;
+
   fSimState := ssTerminated;
-  Tick := 0;
+  fTick := 0;
   fUpdateSimStatus();
   Log('TMainThread-Execute: End');
 end;
